a9751a63204ae667060ab7215facaee9ca2699d2
luiscolorado-tech <luiscolorado@outlook.com>
2021-05-13 22:27:42 -0500

    Add stack nodes

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * is_number - iterates each character of string to check of isdigit
+ * @n: integer
+ * Return: 0 if is number, else -1 if not
+ */
+int is_number(const char *n)
+
+
+void push(stack_t **h, unsigned int line_number, const char *n)
+{
+
+    void pop(stack_t **h, unsigned int line_number)
