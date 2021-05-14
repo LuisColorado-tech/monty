b2807f22caaa7044d9021e3b365c790e0255c736
luiscolorado-tech <luiscolorado@outlook.com>
2021-05-13 21:51:39 -0500

    Add node position

---
+++ nwposition.c
@@ -0,0 +1,7 @@
+#include "monty.h"
+/**
+ * swap - swap locations of previous stack with the top stack
+ * @h: node to be swapped
+ * @line_number: node number
+ */
+void swap(stack_t **h, unsigned int line_number)
