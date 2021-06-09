48ff6ffa0057e927d3fb5c48ee5fb377edd6c16d
LuisColorado-tech <luiscolorado@outlook.com>
2021-06-09 16:06:53 -0500

    Fix and add new files

--- get_op_func.c
+++ get_op_func.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * getfunc - function to pointer to function
- * @token1: 1st bytecode 
- * Return: pointer to function
+ * get_op_func - function to select correct operation function
+ * @token1: 1st bytecode input (opcode)
+ * Return: pointer to correct operation function
  */
 void (*get_op_func(char *token1))(stack_t **stack, unsigned int line_number)
 {
